5c4decf04f1ce8a0711a490d0c736459a77e5cbb
EvModder <evmodder@gmail.com>
2019-04-08 01:10:19 -0700

    deprecated ItemDesc class since all data is now held in Material/type

--- src/Evil_Code_Renewable/ItemDescDeprecated.java
+++ src/Evil_Code_Renewable/ItemDescDeprecated.java
@@ -2,13 +2,14 @@ package Evil_Code_Renewable;
 
 import org.bukkit.Material;
 
-public class ItemDesc{
+public class ItemDescDeprecated{
 	Material mat;
 	byte data;
-	public ItemDesc(Material m, byte b){mat = m; data = b;}
-	public ItemDesc(Material m){mat = m; data = 0;}
+	public ItemDescDeprecated(Material m, byte b){mat = m; data = b;}
+	public ItemDescDeprecated(Material m){mat = m; data = 0;}
 	@Override public boolean equals(Object o){
-		return o instanceof ItemDesc && ((ItemDesc)o).mat == mat && ((ItemDesc)o).data == data;
+		return o instanceof ItemDescDeprecated &&
+				((ItemDescDeprecated)o).mat == mat && ((ItemDescDeprecated)o).data == data;
 	}
 	@Override public int hashCode(){
 		return mat.ordinal();
