6bb4c8c32b0a838f6c6694ba6d8ad41471e1fc57
EvModder <evmodder@gmail.com>
2019-05-02 04:04:21 -0700

    massive 1.14 changes --  so much is renewable now :/

--- src/net/evmodder/Renewable/RenewableChecker.java
+++ src/net/evmodder/Renewable/RenewableChecker.java
@@ -12,60 +12,21 @@ public class RenewableChecker{
 	static final HashSet<Material> rescueList = new HashSet<Material>();
 	static final UnionFind<Material> reversible = new UnionFind<Material>();
 	static{
-		reversible.add(Material.DIRT);
-		reversible.addToSet(Material.GRASS_BLOCK, Material.DIRT);
-		reversible.addToSet(Material.FARMLAND, Material.DIRT);
-		reversible.addToSet(Material.MYCELIUM, Material.DIRT);
-		reversible.addToSet(Material.GRASS_PATH, Material.DIRT);
-		reversible.addToSet(Material.PODZOL, Material.DIRT);
-
-		reversible.add(Material.CLAY);
-		reversible.addToSet(Material.CLAY_BALL, Material.CLAY);
-
 		reversible.add(Material.DIAMOND);
 		reversible.addToSet(Material.DIAMOND_BLOCK, Material.DIAMOND);
 
-//		reversible.add(new ItemDesc(Material.FLOWER_POT));
-//		reversible.addToSet(new ItemDesc(Material.FLOWER_POT_ITEM), new ItemDesc(Material.FLOWER_POT));
-
-//		reversible.add(new ItemDesc(Material.REDSTONE_COMPARATOR));
-//		reversible.addToSet(new ItemDesc(Material.REDSTONE_COMPARATOR_ON), new ItemDesc(Material.REDSTONE_COMPARATOR));
-//		reversible.addToSet(new ItemDesc(Material.REDSTONE_COMPARATOR_OFF), new ItemDesc(Material.REDSTONE_COMPARATOR));
-
-		reversible.add(Material.CHISELED_SANDSTONE);//Chiseled Sandstone & slabs
-		reversible.addToSet(Material.SANDSTONE_SLAB, Material.CHISELED_SANDSTONE);
-		//reversible.addToSet(Material.DOUBLE_SANDSTONE_SLAB, Material.CHISELED_SANDSTONE);
-
-		reversible.add(Material.CHISELED_RED_SANDSTONE);//Red Chiseled Sandstone & slabs
-		reversible.addToSet(Material.RED_SANDSTONE_SLAB, Material.CHISELED_RED_SANDSTONE);
-		//reversible.addToSet(Material.DOUBLE_RED_SANDSTONE_SLAB, Material.CHISELED_RED_SANDSTONE);
-
-		reversible.add(Material.BRICK_SLAB);//Brick slabs & d_slabs
-		//reversible.addToSet(Material.DOUBLE_BRICK_SLAB, Material.BRICK_SLAB);
-
-		reversible.add(Material.NETHER_BRICK_SLAB);//Netherbrick slabs & d_slabs
+		//reversible.add(Material.NETHER_BRICK_SLAB);//Netherbrick slabs & d_slabs
 		//reversible.addToSet(Material.DOUBLE_NETHER_BRICK_SLAB, Material.NETHER_BRICK_SLAB);
-
-		reversible.add(Material.QUARTZ_SLAB);//Chiseled Quartz & Slabs
-		reversible.addToSet(Material.CHISELED_QUARTZ_BLOCK, Material.QUARTZ_SLAB);
-		//reversible.addToSet(Material.DOUBLE_QUARTZ_SLAB, Material.QUARTZ_SLAB);
-
-//		reversible.add(new ItemDesc(Material.PURPUR_SLAB));//Purpur slabs & d_slabs //NOTE: Purpur is renewable!
-//		reversible.addToSet(new ItemDesc(Material.PURPUR_DOUBLE_SLAB), new ItemDesc(Material.PURPUR_SLAB));
 
 		reversible.add(Material.SPONGE);//Sponge & WetSponge
 		reversible.addToSet(Material.WET_SPONGE, Material.SPONGE);
-
-		reversible.add(Material.TNT);//TNT & TNT Minecart
-		reversible.addToSet(Material.TNT_MINECART, Material.TNT);
 	}
 
-	final boolean UNRENEWABLE_LAVA, UNRENEWABLE_DIA_ARMOR, UNRENEWABLE_MOBS, UNRENEWABLE_GRAVITY;
-	final boolean INCLUDE_UNOBT, OBT_SPAWNERS, OBT_MOB_EGGS, OBT_INFESTED, OBT_SMOOTH_BRICKS, OBT_CMD_BLOCKS,
-					OBT_BEDROCK, OBT_END_PORTALS, OBT_BARRIERS, OBT_STRUCTURE_BLOCKS;
+	final boolean UNRENEWABLE_LAVA, UNRENEWABLE_MOBS, UNRENEWABLE_GRAVITY;
+	final boolean INCLUDE_UNOBT, OBT_SPAWNERS, OBT_MOB_EGGS, OBT_INFESTED, OBT_CMD_BLOCKS,
+					OBT_BEDROCK, OBT_END_PORTALS, OBT_BARRIERS, OBT_STRUCTURE_BLOCKS, OBT_PETRIFIED_SLABS;
 	RenewableChecker(Renewable pl){
 		UNRENEWABLE_LAVA = !pl.getConfig().getBoolean("renewable-lava", true);
-		UNRENEWABLE_DIA_ARMOR = !pl.getConfig().getBoolean("renewable-diamond-armor", true);
 		UNRENEWABLE_MOBS =  !pl.getConfig().getBoolean("renewable-mob-drops", false);
 		UNRENEWABLE_GRAVITY = !pl.getConfig().getBoolean("renewable-gravity-blocks", false);
 		//
@@ -73,12 +34,12 @@ public class RenewableChecker{
 		OBT_SPAWNERS = pl.getConfig().getBoolean("spawners-obtainable", false);
 		OBT_MOB_EGGS = pl.getConfig().getBoolean("spawn-eggs-obtainable", false);
 		OBT_INFESTED = pl.getConfig().getBoolean("infested-blocks-obtainable", false);
-		OBT_SMOOTH_BRICKS = pl.getConfig().getBoolean("smooth-bricks-obtainable", false);
 		OBT_CMD_BLOCKS = pl.getConfig().getBoolean("command-blocks-obtainable", false);
 		OBT_BEDROCK = pl.getConfig().getBoolean("bedrock-obtainable", false);
 		OBT_END_PORTALS = pl.getConfig().getBoolean("end-portals-obtainable", false);
 		OBT_BARRIERS = pl.getConfig().getBoolean("barriers-obtainable", false); 
 		OBT_STRUCTURE_BLOCKS = pl.getConfig().getBoolean("structure-blocks-obtainable", false);
+		OBT_PETRIFIED_SLABS = pl.getConfig().getBoolean("petrified-slabs-obtainable", false);
 		//
 		for(String name : pl.getConfig().getStringList("rescued-renewables")){
 			try{ rescueList.add(Material.valueOf(name.toUpperCase())); }
@@ -92,12 +53,6 @@ public class RenewableChecker{
 		//Note: (Somewhat) Sorted by ID, from least to greatest
 		switch(item.getType()){
 			case DIAMOND:
-			case DIAMOND_SHOVEL:
-			case DIAMOND_HOE:
-			case BRICK:
-			case CLAY_BALL:
-//			case LAPIS_LAZULI://Note: renewable (villagers)
-//			case GLASS_BOTTLE://Note: renewable (villagers & witches)
 			case NETHER_BRICK:
 			case QUARTZ:
 			case IRON_HORSE_ARMOR:
@@ -111,11 +66,6 @@ public class RenewableChecker{
 				return UNRENEWABLE_MOBS && !OBT_MOB_EGGS;
 			case NETHER_STAR:
 				return UNRENEWABLE_MOBS;
-			case DIAMOND_HELMET:
-//			case DIAMOND_CHESTPLATE://Note: renewable (villagers)
-			case DIAMOND_LEGGINGS:
-			case DIAMOND_BOOTS:
-				return UNRENEWABLE_DIA_ARMOR;
 			case FLINT:
 			case FLINT_AND_STEEL:
 			case TNT_MINECART:
@@ -138,15 +88,28 @@ public class RenewableChecker{
 
 		//Note: (Somewhat) Sorted by ID, from least to greatest
 		switch(mat){
+			case TERRACOTTA://Note: only uncolored terracotta is unrenewable
 			case GRANITE:
+			case GRANITE_SLAB:
+			case GRANITE_STAIRS:
+			case GRANITE_WALL:
+			case POLISHED_GRANITE:
+			case POLISHED_GRANITE_SLAB:
+			case POLISHED_GRANITE_STAIRS:
 			case DIORITE:
+			case DIORITE_SLAB:
+			case DIORITE_STAIRS:
+			case DIORITE_WALL:
+			case POLISHED_DIORITE:
+			case POLISHED_DIORITE_SLAB:
+			case POLISHED_DIORITE_STAIRS:
 			case ANDESITE:
-			case GRASS_BLOCK:
-			case GRASS_PATH:
-			case FARMLAND:
-			case DIRT:
-			case PODZOL:
-			case COARSE_DIRT:
+			case ANDESITE_SLAB:
+			case ANDESITE_STAIRS:
+			case ANDESITE_WALL:
+			case POLISHED_ANDESITE:
+			case POLISHED_ANDESITE_SLAB:
+			case POLISHED_ANDESITE_STAIRS:
 			case SPONGE:
 //			case GLASS://Note: glass is renewable! (Villagers)
 			case ENCHANTING_TABLE:
@@ -158,43 +121,35 @@ public class RenewableChecker{
 			case DAYLIGHT_DETECTOR:
 			case COBWEB:
 			case DEAD_BUSH:
-//			case MOSSY_COBBLESTONE://Note: renewable! (Vines)
-			case CLAY:
-			case BRICKS:
-			case BRICK_STAIRS://Note: Same (red) brick type as above, just as stairs
 			case NETHERRACK:
 			case SOUL_SAND:
-			case MYCELIUM://Note: Since dirt is unrenewable, this is as well.
-			case NETHER_BRICK:
-			case NETHER_BRICK_STAIRS:
+			case NETHER_BRICKS:
 			case NETHER_BRICK_FENCE:
+			case NETHER_BRICK_SLAB:
+			case NETHER_BRICK_STAIRS:
+			case NETHER_BRICK_WALL:
 			case RED_NETHER_BRICKS:
+			case RED_NETHER_BRICK_SLAB:
+			case RED_NETHER_BRICK_STAIRS:
+			case RED_NETHER_BRICK_WALL:
 //			case ENDSTONE://Note: renewable! :o (When dragon is respawned, endstone under platform)
 			case QUARTZ_BLOCK:
-			case QUARTZ_STAIRS:
-//			case PACKED_ICE://Note: craftable with ice in 1.13
+			case QUARTZ_STAIRS://Note: slab, pillar, and chiseled are renewable
+			case SMOOTH_QUARTZ:
+			case SMOOTH_QUARTZ_SLAB:
+			case SMOOTH_QUARTZ_STAIRS:
 //			case MAGMA_BLOCK://Note: renewable! (4 magma cream)
 				return true;
-			case SAND://Note: Sand and Red Sand are considered unrenewable.
+//			case SAND://Note: renewable in 1.14! (wandering trader)
+//			case RED_SAND://Note: ^
 			case GRAVEL:
+			case FLETCHING_TABLE:
 			case DRAGON_EGG:
-			case DRAGON_HEAD:
-			case SANDSTONE:
-			case RED_SANDSTONE:
-			case SANDSTONE_SLAB:
-			case RED_SANDSTONE_SLAB:
-			case SANDSTONE_STAIRS:
-			case RED_SANDSTONE_STAIRS:
-			case TNT:
 				return UNRENEWABLE_GRAVITY;
 			case LAVA://Flowing lava is renewable
 				return UNRENEWABLE_LAVA && ((Levelled)data).getLevel() == 0;
 			case BEACON:
 				return UNRENEWABLE_MOBS;
-			case SMOOTH_SANDSTONE://TODO: these will be renewable (via smelt) in 1.14!
-			case SMOOTH_RED_SANDSTONE://TODO: ^
-			case SMOOTH_STONE://TODO: ^
-				return INCLUDE_UNOBT && !OBT_SMOOTH_BRICKS;
 			case SPAWNER:
 				return INCLUDE_UNOBT && !OBT_SPAWNERS;
 			case BEDROCK:
@@ -210,12 +165,15 @@ public class RenewableChecker{
 				return INCLUDE_UNOBT && !OBT_CMD_BLOCKS;
 			case STRUCTURE_BLOCK:
 			case STRUCTURE_VOID:
+			case JIGSAW:
 				return INCLUDE_UNOBT && !OBT_STRUCTURE_BLOCKS;
+			case PETRIFIED_OAK_SLAB:
+				return INCLUDE_UNOBT && !OBT_PETRIFIED_SLABS;
 			default:
-				if(TypeUtils.isConcrete(mat) || TypeUtils.isConcretePowder(mat)) return UNRENEWABLE_GRAVITY;
+				if(TypeUtils.isConcretePowder(mat)) return UNRENEWABLE_GRAVITY;
 				if(TypeUtils.isShulkerBox(mat)) return UNRENEWABLE_MOBS;
 				if(TypeUtils.isInfested(mat)) return INCLUDE_UNOBT && !OBT_INFESTED;
-				return TypeUtils.isOre(mat) || TypeUtils.isTerracotta(mat) || TypeUtils.isGlazedTerracotta(mat);
+				return TypeUtils.isOre(mat);
 		}
 	}
 
--- src/net/evmodder/Renewable/RenewableStandardizer.java
+++ src/net/evmodder/Renewable/RenewableStandardizer.java
@@ -8,12 +8,11 @@ import net.evmodder.EvLib.FileIO;
 import net.evmodder.EvLib.Fraction;
 import net.evmodder.EvLib.TypeUtils;
 
-class RenewableStandardizer{
-	final boolean STD_DIRT, STD_LORE, STD_NAME, STD_ENCHANTS, STD_FLAGS, STD_OTHER_META;
+class RenewableStandardizer{//TODO: standardize slabs/stairs using stone-cutter values (not crafting table costs)
+	final boolean STD_LORE, STD_NAME, STD_ENCHANTS, STD_FLAGS, STD_OTHER_META;
 	final static HashMap<Material, Fraction> rescuedParts = new HashMap<Material, Fraction>();
 	static{
 		rescuedParts.put(Material.QUARTZ, new Fraction(0, 2));
-		rescuedParts.put(Material.SAND, new Fraction(0, 2));
 		rescuedParts.put(Material.GRAVEL, new Fraction(0, 2));
 		rescuedParts.put(Material.NETHERRACK, new Fraction(0, 2));
 		rescuedParts.put(Material.DIAMOND_ORE, new Fraction(0, 9));
@@ -22,7 +21,6 @@ class RenewableStandardizer{
 
 	RenewableStandardizer(Renewable pl){
 		this.pl = pl;
-		STD_DIRT = pl.getConfig().getBoolean("dirt-standardizes-to-gravel", true);
 		STD_LORE = pl.getConfig().getBoolean("standardize-if-has-lore", false);
 		STD_NAME = pl.getConfig().getBoolean("standardize-if-has-name", true);
 		STD_ENCHANTS = pl.getConfig().getBoolean("standardize-if-has-enchants", true);
@@ -62,94 +60,55 @@ class RenewableStandardizer{
 		Material type = item.getType();
 
 		switch(type){
-			case TERRACOTTA:
-			case CLAY:
-			case BRICKS:
-				return new ItemStack(Material.CLAY_BALL, item.getAmount()*4);
-			case BRICK:
-				return new ItemStack(Material.CLAY_BALL, item.getAmount());
 			case FLINT:
 			case FLINT_AND_STEEL:
 				return new ItemStack(Material.GRAVEL, item.getAmount());
+			case FLETCHING_TABLE:
+				return new ItemStack(Material.GRAVEL, item.getAmount()*2);
 			case DIAMOND_SHOVEL:
 			case JUKEBOX:
 				return new ItemStack(Material.DIAMOND, item.getAmount());
-			case DIAMOND_HOE:
 			case ENCHANTING_TABLE:
 				return new ItemStack(Material.DIAMOND, item.getAmount()*2);
-			case DIAMOND_BOOTS:
-				return new ItemStack(Material.DIAMOND, item.getAmount()*4);
-			case DIAMOND_HELMET:
-				return new ItemStack(Material.DIAMOND, item.getAmount()*5);
-			case DIAMOND_LEGGINGS:
-				return new ItemStack(Material.DIAMOND, item.getAmount()*7);
-//			case DIAMOND_CHESTPLATE:
-//				return new ItemStack(Material.DIAMOND, item.getAmount()*8);//Note: renewable -- villagers
-//			case DIAMOND_ORE:
-//				return new ItemStack(Material.DIAMOND, item.getAmount()*maxDiaPerOre);
-//			case DIAMOND:
-//				rescuedParts.get(Material.DIAMOND_ORE).add(item.getAmount(), maxDiaPerOre);
-//				return new ItemStack(Material.DIAMOND_ORE, rescuedParts.get(Material.DIAMOND_ORE).take1s());
 			case DIAMOND_BLOCK:
 				return new ItemStack(Material.DIAMOND, item.getAmount()*9);
 			case NETHER_STAR:
 			case BEACON:
 				return new ItemStack(Material.SOUL_SAND, item.getAmount()*4);
-			case QUARTZ_BLOCK:
-				return new ItemStack(Material.QUARTZ, item.getAmount()*4);
-			case QUARTZ_STAIRS:
-				return new ItemStack(Material.QUARTZ, item.getAmount()*6);
 			case COMPARATOR:
 			case OBSERVER:
 				return new ItemStack(Material.QUARTZ, item.getAmount());
+			case SMOOTH_QUARTZ_SLAB:
+				return new ItemStack(Material.QUARTZ, item.getAmount()*2);
 			case DAYLIGHT_DETECTOR:
 				return new ItemStack(Material.QUARTZ, item.getAmount()*3);
-			case TNT:
-			case TNT_MINECART:
-			case SANDSTONE:
-				return new ItemStack(Material.SAND, item.getAmount()*4);
-			case RED_SANDSTONE:
-				return new ItemStack(Material.RED_SAND, item.getAmount()*4);
-			case SANDSTONE_STAIRS:
-				return new ItemStack(Material.SAND, item.getAmount()*6);
-			case RED_SANDSTONE_STAIRS:
-				return new ItemStack(Material.RED_SAND, item.getAmount()*6);
-			case NETHER_BRICKS:
-			case NETHER_BRICK_FENCE:
-				return new ItemStack(Material.NETHERRACK, item.getAmount()*4);
-			case NETHER_BRICK:
-				return new ItemStack(Material.NETHERRACK, item.getAmount());
+			case QUARTZ_BLOCK:
+			case SMOOTH_QUARTZ:
+				return new ItemStack(Material.QUARTZ, item.getAmount()*4);
+			case QUARTZ_STAIRS:
+			case SMOOTH_QUARTZ_STAIRS:
+				return new ItemStack(Material.QUARTZ, item.getAmount()*6);
 			case NETHER_BRICK_STAIRS:
-				if(ignoreLeftovers) return item;
-				rescuedParts.get(Material.NETHERRACK).add(item.getAmount()*3, 2);
-				return new ItemStack(Material.NETHERRACK, rescuedParts.get(Material.NETHERRACK).take1s());
-			case RED_NETHER_BRICKS:
-				if(ignoreLeftovers) return item;
-				rescuedParts.get(Material.NETHERRACK).add(item.getAmount(), 2);
-				return new ItemStack(Material.NETHERRACK, rescuedParts.get(Material.NETHERRACK).take1s());
+				return new ItemStack(Material.NETHERRACK, item.getAmount()*6);
+			case NETHER_BRICKS:
+			case NETHER_BRICK_WALL:
+				return new ItemStack(Material.NETHERRACK, item.getAmount()*4);
+			case NETHER_BRICK_FENCE:
+			case RED_NETHER_BRICK_STAIRS:
+				return new ItemStack(Material.NETHERRACK, item.getAmount()*3);
 			case NETHER_BRICK_SLAB:
-				if(ignoreLeftovers) return item;
-				rescuedParts.get(Material.NETHERRACK).add(item.getAmount(), 2);
-				return new ItemStack(Material.NETHERRACK, rescuedParts.get(Material.NETHERRACK).take1s());
-			case QUARTZ_SLAB:
-				return new ItemStack(Material.QUARTZ, item.getAmount()*2);
-			case SPONGE:
+			case RED_NETHER_BRICKS:
+			case RED_NETHER_BRICK_WALL:
+				return new ItemStack(Material.NETHERRACK, item.getAmount()*2);
+			case NETHER_BRICK:
+			case RED_NETHER_BRICK_SLAB:
+				return new ItemStack(Material.NETHERRACK, item.getAmount());
 			case WET_SPONGE:
 				return new ItemStack(Material.SPONGE, item.getAmount());
-			case GRASS_BLOCK:
-			case GRASS_PATH:
-			case FARMLAND:
-			case DIRT:
-			case PODZOL:
-				return new ItemStack(STD_DIRT ? Material.GRAVEL : Material.DIRT, item.getAmount());
 			case COARSE_DIRT:
-				if(STD_DIRT) return new ItemStack(Material.GRAVEL, item.getAmount());
-				if(ignoreLeftovers) return item;
-				rescuedParts.get(Material.DIRT).add(item.getAmount(), 2);
 				rescuedParts.get(Material.GRAVEL).add(item.getAmount(), 2);
 				int gravel = rescuedParts.get(Material.GRAVEL).take1s();
 				if(gravel != 0) return new ItemStack(Material.GRAVEL, gravel);
-				else return new ItemStack(Material.DIRT, rescuedParts.get(Material.DIRT).take1s());
 			case GRANITE:
 			case POLISHED_GRANITE:
 				return new ItemStack(Material.QUARTZ, item.getAmount()*2);
@@ -165,19 +124,12 @@ class RenewableStandardizer{
 				if(TypeUtils.isShulkerBox(type)){
 					return new ItemStack(Material.SHULKER_SHELL, item.getAmount()*2);
 				}
-				if(TypeUtils.isTerracotta(type) || TypeUtils.isGlazedTerracotta(type)){
-					return new ItemStack(Material.CLAY_BALL, item.getAmount()*4);
-				}
-				if(TypeUtils.isConcrete(type) || TypeUtils.isConcretePowder(type)){
+				if(TypeUtils.isConcretePowder(type)){
 					if(ignoreLeftovers) return item;
-					rescuedParts.get(Material.SAND).add(item.getAmount(), 2);
 					rescuedParts.get(Material.GRAVEL).add(item.getAmount(), 2);
 					gravel = rescuedParts.get(Material.GRAVEL).take1s();
 					if(gravel != 0) return new ItemStack(Material.GRAVEL, gravel);
-					else return new ItemStack(Material.SAND, rescuedParts.get(Material.SAND).take1s());
 				}
-				if(TypeUtils.isFlowerPot(type))
-					return new ItemStack(Material.CLAY_BALL, item.getAmount()*3);
 				return item;
 		}
 	}
