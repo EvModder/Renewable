d564e669fba25483a35c696d16dcd11adba67303
EvModder <evmodder@gmail.com>
2022-06-13 06:08:51 -0700

    Fix detection for items falling into void

--- src/net/evmodder/Renewable/listeners/ItemDeathListener.java
+++ src/net/evmodder/Renewable/listeners/ItemDeathListener.java
@@ -1,13 +1,18 @@
 package net.evmodder.Renewable.listeners;
 
+import org.bukkit.World.Environment;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityEvent;
 import org.bukkit.event.entity.ItemDespawnEvent;
 import org.bukkit.event.player.PlayerItemBreakEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.EventExecutor;
 import net.evmodder.Renewable.ItemTaggingUtil;
 import net.evmodder.Renewable.Renewable;
 
@@ -18,6 +23,25 @@ public class ItemDeathListener implements Listener{
 	public ItemDeathListener(){
 		plugin = Renewable.getPlugin();
 		saveItems = Renewable.getPlugin().getConfig().getBoolean("rescue-items", true);
+
+		try{
+			@SuppressWarnings("unchecked")
+			Class<? extends Event> clazz = (Class<? extends Event>)
+				Class.forName("com.destroystokyo.paper.event.entity.EntityRemoveFromWorldEvent");
+			plugin.getServer().getPluginManager().registerEvent(clazz, this, EventPriority.MONITOR, new EventExecutor(){
+				@Override public void execute(Listener listener, Event event){
+					Entity entity = ((EntityEvent)event).getEntity();
+					if(entity instanceof Item && entity.getLocation().getY() < (entity.getWorld().getEnvironment() == Environment.NORMAL ? -127 : -63)
+							&& plugin.getAPI().isUnrenewable(((Item)entity).getItemStack())){
+						final ItemStack item = ((Item)entity).getItemStack();
+						plugin.getLogger().fine("Item fell into void: "+entity.getLocation().toString());
+						plugin.getAPI().punish(ItemTaggingUtil.getLastPlayerInContact(item), item.getType());
+						if(saveItems) plugin.getAPI().rescueItem(item);
+					}
+				}
+			}, plugin);
+		}
+		catch(ClassNotFoundException e){}
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR)
@@ -49,7 +73,7 @@ public class ItemDeathListener implements Listener{
 	}*/
 
 
-	//Includes damage from: explosion, void
+	//All damage except despawn & void:
 	@EventHandler(priority = EventPriority.MONITOR)
 	public void onItemMiscDamage(EntityDamageEvent evt){
 		if(!evt.isCancelled() && evt.getEntity() instanceof Item
